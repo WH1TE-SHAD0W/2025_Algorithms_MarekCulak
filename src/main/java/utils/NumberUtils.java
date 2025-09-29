e64cc764b50e6538a028120ac1c92af27ee1b27f
D00271292 <d00271292@student.dkit.ie>
2025-09-29 14:35:03 +0100

    feat: method getPos working

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -5,12 +5,16 @@ public class NumberUtils {
      * Returns first found position of a number specified within supplied array.
      * @param arr, an array in which to look for
      * @param num, the number to find in an array
-     * @return int index, returns the index of num in arr. Example
+     * @return int index, returns the index of num in arr. Returns -1 if not found. Example:
      * Input: ([0,5,3,1], 5)
      * Output: 1
      */
     public static int getPos(int[] arr, int num) {
-        int index = 0;
-        return index;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
