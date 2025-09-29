4a101fee6466709dc90fb6139e125ea52f2ebbde
D00271292 <d00271292@student.dkit.ie>
2025-09-29 14:38:07 +0100

    feat: method getLastPos with docs

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -17,4 +17,26 @@ public class NumberUtils {
         }
         return -1;
     }
+
+    /**
+     * Returns last found position of a number specified within supplied array.
+     * @param arr, an array in which to look for
+     * @param num, the number to find in an array
+     * @return int index, returns the index of num in arr. Returns -1 if not found. Example:
+     * Input: ([0,5,3,1,5], 5)
+     * Output: 4
+     */
+    public static int getLastPos(int[] arr, int num) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    static void main() {
+        System.out.println(getLastPos(new int[]{0,5,3,1,5}, 5));
+    }
 }
